c0a617b255f9be7370cb0541a2c481c14e03e8b3
Maxim <lolerpro4@gmail.com>
2024-06-11 20:43:13 +0300

    [feet]: Added routes

---
+++ internal/server/routes.go
@@ -0,0 +1,38 @@
+package server
+
+import (
+	"chermo_admin/internal/parser"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (s *Server) RegisterRoutes() http.Handler {
+	r := gin.Default()
+
+	r.GET("/", s.HelloWorldHandler)
+	r.GET("/events", s.getEventsHandler)
+	r.GET("/health", s.healthHandler)
+
+	return r
+}
+
+func (s *Server) HelloWorldHandler(c *gin.Context) {
+	resp := make(map[string]string)
+	resp["message"] = "Hello World"
+
+	c.JSON(http.StatusOK, resp)
+}
+
+func (s *Server) healthHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, s.db.Health())
+}
+
+func (s *Server) getEventsHandler(c *gin.Context) {
+	items, err := parser.FetchEvents()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, items)
+}
